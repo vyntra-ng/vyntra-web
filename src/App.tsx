99971da5cd157dc25bab854433a2089777c7b9b2
vyntra-ng <your-github-email@example.com>
2026-09-06 02:20:20 +0100

    did stuff on privacy and term and home

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Layout from './components/Layout';
 import Home from './pages/Home';
 import Terms from './pages/Terms';
 import Privacy from './pages/Privacy';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
           <Route index element={<Home />} />
           <Route path="terms" element={<Terms />} />
           <Route path="privacy" element={<Privacy />} />
+          {/* Catch-all route for the 404 Match Not Found page */}
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
